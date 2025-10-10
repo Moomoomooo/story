ce06e89c05781736e979d3b822021a483e75568b
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-10 08:42:33 -0400

    Update GameScreen.ts

--- src/gui/GameScreen.ts
+++ src/gui/GameScreen.ts
@@ -1,12 +1,14 @@
 import * as THREE from "three";
 
-const scene = new THREE.scene();
-const camera = THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
-const renderer = new THREE.WebGLRenderer();
+const scene: Scene = new THREE.scene();
+const camera: PerspectiveCamera = THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
+const renderer: WebGLRenderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 window.addEventListener('resize', (event): void => {
-  camera
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 })
 
