307ef63984e6b9aaf55810820e13170a4d21bea6
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-14 09:41:51 -0400

    Update Niam.ts

--- src/data/player/Niam.ts
+++ src/data/player/Niam.ts
@@ -1,3 +1,28 @@
+import { DivinityLevel } from '#enums/DivinityLevel.ts';
+
+export const niam = {
+  level: 1,
+  hp: 0,
+  mp: 0,
+  presence: 5, // governs hp and resistance to curses
+  acuity: 5, // governs setup window time, critical chance, and
+  
+  grace: 5, // governs mp and 
+  divinity: 0,
 
 
-export const niam: Entity = new Entity()
+  initialize: function(): void {
+    this.hp = ;
+    this.mp = ;
+    
+  },
+  
+}
+
+export interface Player {
+  level: number;
+  acuity: number;
+  grace: number;
+  divinity: DivinityLevel;
+  
+}
