3d99bf6eb082cccfa884f9f5b49cf9d2eb1a7ed9
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-17 08:37:09 -0400

    Update Niam.ts

--- src/data/player/Niam.ts
+++ src/data/player/Niam.ts
@@ -3,7 +3,7 @@ import { DivinityLevel } from '#enums/DivinityLevel.ts';
 export const niam = {
   level: 1,
   hp: 0,
-  mp: 0,
+  sp: 0,
   presence: 5, // governs hp and resistance to curses
   acuity: 5, // governs setup window time, critical chance, and
   
