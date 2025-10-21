dd08feb461f8c7ded2367a5f9a457b65625a7a3a
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-21 09:49:54 -0400

    Update and rename RegionId.ts to WorldId.ts

--- src/enums/RegionId.ts
+++
@@ -1,4 +0,0 @@
-export enum RegionId {
-  EIELA, //
-  VALQUERA,
-}
---
+++ src/enums/WorldId.ts
@@ -0,0 +1,10 @@
+export enum WorldId {
+  EIELA, // the flourishing lands, realm of the Grandmother Oak, home of the einherjar. Wreathed in gold and leaves. 
+  VALQUERA, // The 
+  NUMERAN, // the lands,
+  NOD, // the dream lands,  a beautiful pastel paradise. Vanahiem parralell/faerie world. 
+  DOLERA, // The infected lands
+  ANILID, // 
+  EWELEN, // the dolorous lands, 
+  
+}
