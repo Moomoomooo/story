0acfebe9cfc67033f8514e44b06e396d26a8ac8d
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-22 12:38:34 -0400

    Update Renderer.ts

--- src2/Renderer.ts
+++ src2/Renderer.ts
@@ -1,7 +1,6 @@
 /* The game needs to send out a tick for every frame. Other structures in the program 
 listen for this */
-
-import * from THREE.js as 'three';
+import * as THREE from "three";
 
 export class Renderer {
   
