8308fe51af01845285aaa700b6c8174d85a395c7
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-23 08:24:13 -0400

    Update Renderer.ts

--- src2/Renderer.ts
+++ src2/Renderer.ts
@@ -2,6 +2,16 @@
 listen for this */
 import * as THREE from "three";
 
-export class Renderer {
+export class gameScreen {
+  camera: THREE.PerspectiveCamera;
+  scene: THREE.Scene;
+  renderer: THREE.WebGLRenderer;
+  ticker: number;
+
+  constructor() {
+    this.scene = new THREE.Scene;
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.Renderer = new THREE.WebGLRenderer
+  }
   
 }
