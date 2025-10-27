f9beb8c8055be8f0d617ffc490b4bec60f4ee661
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-27 08:24:45 -0400

    Update HUD.ts

--- src2/gui/HUD.ts
+++ src2/gui/HUD.ts
@@ -1,3 +1,17 @@
-export class HUD {
+export const HUD = {
+  hud: document.getElementById("hud"),
+  healthBar: document.createElement("div"),
+  spiritBar: document.createElement("div"),
   
+  skillSelectScreen: document.createElement("div"),
+
+  initialize: function() {
+  },
+
+  hide: function() {
+    this.hud.style.display = 'none';
+  },
+  show: function() {
+    this.hud.style.display = 'block';
+  }
 }
